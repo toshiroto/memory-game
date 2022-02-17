d75700bc725e923a6dc7e4d38f83f9ea94e417e3
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-17 14:34:39 +0900

    date framework aded

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipcard() {
   this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipcard));
