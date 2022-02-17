8de579993592125bf3878e83b585dc5ecac58a42
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-17 14:54:51 +0900

    Same Card Click JS rules

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
+
 function flipcard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
   this.classList.toggle('flip');
   this.classList.add('flip');
 
@@ -13,28 +17,38 @@ function flipcard() {
     return;
   }
   secondCard = this;
-  hasFlippedCard = false;
+
 
   checkForMatch();
+}
 
   function checkForMatch() {
-    if(firstCard.dataset.framework === secondCard.dataset.framework) {
-      disableCards();
-      return;
-    }
-    unflipCards();
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
   }
+
   function disableCards() {
     firstCard.removeEventListener('click', flipcard);
     secondCard.removeEventListener('click', flipcard);
+
+    resetBoard();
   }
 
   function unflipCards() {
+      lockBoard = true;
+
     setTimeout(() => {
       firstCard.classList.remove('flip');
       secondCard.classList.remove('flip')
+
+      resetBoard();
     }, 1500);
   }
+
+  function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
+
 
 cards.forEach(card => card.addEventListener('click', flipcard));
